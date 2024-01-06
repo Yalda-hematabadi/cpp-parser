c530766505ef6c98d4567bb0aece7860a61be52c
HematAbadi <yaldahmtabdi81@gmail.com>
2024-01-06 17:43:45 +0330

    initializing parser

---
+++ Parser.py
@@ -0,0 +1,8 @@
+from First_Follow import First_Follow
+from Scanner import Scanner
+
+
+class Parser():
+    def __init__():
+        self.scanner = Scanner.Scanner()
+        self.symbol_table = Scanner.SymbolTable()
